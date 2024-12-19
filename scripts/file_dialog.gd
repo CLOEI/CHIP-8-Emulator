3e1dcc5c83231b5393dbb8cb531c3d708c1e7376
Cendy <cendyemail@gmail.com>
2024-12-19 15:48:37 +0700

    feat: scene change

--- scripts/file_dialog.gd
+++ scripts/file_dialog.gd
@@ -4,7 +4,8 @@ var file_path = ""
 
 func _on_button_pressed() -> void:
 	self.visible = true
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_file_selected(path: String) -> void:
-	file_path = path
+	GlobalFileDialog.file_path = path
